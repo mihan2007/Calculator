5e4a69329528146c2a01488087efb4a48479e957
mihan2007 <52691119+mihan2007@users.noreply.github.com>
2022-12-05 18:43:02 +0300

    finish calculator

--- Assets/scripts/Calculator.cs
+++ Assets/scripts/Calculator.cs
@@ -8,35 +8,75 @@ public class Calculator : MonoBehaviour
 
     public TMP_InputField InputField1;
     public TMP_InputField InputField2;
-    private float _number1;
-    //public string Result;
+    public TMP_InputField ResultField;
+    private float _number1 = 0;
+    private float _number2 = 0;
+    public float Result;
 
-    
+
 
     public void ReadIputField1(string s)
     {
-        s = InputField1.text; // считываем значение из текстового поля 1 
-        if (float.TryParse(s, out float result)) // проверяем является введенное значение числом
+        s = InputField1.text; 
+        if (float.TryParse(s, out float value)) // проверяем является введенное значение числом
         {
-            _number1 = float.Parse(s);      // если да то перобразуем строку в число 
+            //_number1 = float.Parse(s);
+            _number1 = value;
         }
         else 
         {
             _number1 = 0;       // если поле пустое или введено не число приравниваем число к нулю 
         }
-
-        Debug.Log(s);
+         //Debug.Log(_number1);
     }
 
     public void ReadIputField2(string s2)
     {
         s2 = InputField2.text;
-        Debug.Log(s2);
+        if (float.TryParse(s2, out float value2))
+        {
+            _number2 = value2;
+        }
+        else
+        {
+            _number2 = 0;
+        }
     }
 
-    public void ClickOperation(string val) 
-     {
-        Debug.Log(message: $" ClickOperatio val: {val}");
-//        input1.text = $"nk";
-     }
+    public void Multiply()
+    {
+        Result = _number1 * _number2;
+        Debug.Log(Result);
+        ResultField.text = Result.ToString();
+
+    }
+
+    public void Minus ()
+    {
+        Result = _number1 - _number2;
+        Debug.Log(Result);
+        ResultField.text = Result.ToString();
+    }
+
+    public void Plus()
+    {
+        Result = _number1 + _number2;
+        Debug.Log(Result);
+        ResultField.text = Result.ToString();
+    }
+
+    public void Divide() 
+    {
+        if (_number2 != 0)
+        {
+            Result = _number1 / _number2;
+            Debug.Log(Result);
+            ResultField.text = Result.ToString();
+        }
+        else 
+        {
+            ResultField.text = "You can't divide by zero";
+        }
+    }
+
 }
