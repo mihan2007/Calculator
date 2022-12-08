fdc19668d4abc796c9deb7463858ea47441bb829
mihan2007 <52691119+mihan2007@users.noreply.github.com>
2022-12-08 21:31:02 +0300

    drop code
    arithmetic functions not working properly

--- Assets/scripts/Calculator.cs
+++ Assets/scripts/Calculator.cs
@@ -8,24 +8,32 @@ public class Calculator : MonoBehaviour
     [SerializeField] private TMP_InputField _inputField1;
     [SerializeField] private TMP_InputField _inputField2;
     [SerializeField] private TMP_InputField _resultField;
-   
-    private float _number1 = 0;
-    private float _number2 = 0;
     [SerializeField] private float _result;
-    [SerializeField] private bool _ñomma = false;
+    [SerializeField] private bool _comma = false;
     [SerializeField] private bool _screenInputMode = true;
+    [SerializeField] private int _operationID;
+    [SerializeField] private int _preOperationID;
+
+    public float _number1 = 0;
+    public float _number2 = 0;
+
+    public void ChangeFocus()
+    {
+        _inputField1.ActivateInputField();
+    }
     public void ReadInputField1(string rawValue)
     {
-        rawValue = _inputField1.text; 
-        if (float.TryParse(rawValue, out float value))
+        //_number1 = float.Parse(rawValue);
+
+        if (float.TryParse(rawValue, out float value2))
         {
-            _number1 = value;
+            _number1 = value2;
         }
-        else 
+        else
         {
             _number1 = 0;
-            _inputField1.text = _number1.ToString();
         }
+
     }
     public void ReadInputField2(string rawValue)
     {
@@ -41,28 +49,32 @@ public class Calculator : MonoBehaviour
     }
     public void InputNumberOnClick(int ButtonNumber)
     {
-        _ñomma = _inputField1.text.Contains(",");
-        
-        if (_screenInputMode == false) 
+        _comma = _inputField1.text.Contains(",");
+
+        if (_screenInputMode == false)
         {
             _inputField1.text = "";
             _screenInputMode = true;
         }
+        if (_operationID == 3) 
+        {
 
-        if (ButtonNumber != 34)  // the number 34 is ID number of _ñomma button
+        }
+
+        if (ButtonNumber != 34)  // the number 34 is ID number of _comma button
         {
             _inputField1.text = _inputField1.text + ButtonNumber.ToString();
         }
-        else if (ButtonNumber == 34 && _ñomma == false)
+        else if (ButtonNumber == 34 && _comma == false)
         {
             _inputField1.text = _inputField1.text + ",";
-            _ñomma = true;
+            _comma = true;
         }
     }
 
-    public void OperationOnClick (string operaion)
+    public void OperationOnClick(string operaion)
     {
-        
+
         Debug.Log(operaion);
         switch (operaion)
         {
@@ -71,32 +83,7 @@ public class Calculator : MonoBehaviour
                 _inputField1.text = "";
                 _inputField2.text = "";
                 break;
-            case "-1":
-                _result = float.Parse(_inputField1.text)* (-1);
-                _inputField1.text = _result.ToString();
-                break;
 
-            case "+":
-                    if (_result == 0)
-                {
-                    _result = float.Parse(_inputField1.text);
-                    _inputField2.text = _result.ToString() + "+";
-                    _screenInputMode = false;
-                }
-                    else
-                {
-                    _result = _result + float.Parse(_inputField1.text);
-                    _inputField2.text = _inputField2.text  + _inputField1.text + "+";
-                    _inputField1.text = _result.ToString();
-                    _screenInputMode = false;
-                }
-
-                break;
-            case "=":
-                _inputField1.text = _result.ToString();
-                _inputField2.text = "";
-                _screenInputMode = false;
-                break;
         }
 
 
@@ -106,26 +93,58 @@ public class Calculator : MonoBehaviour
         _result = _number1 * _number2;
         _resultField.text = _result.ToString();
     }
-    public void Minus()
-    {
-        _result = _number1 - _number2;
-        _resultField.text = _result.ToString();
-    }
+
     public void Plus()
     {
-        _result = _number1 + _number2;
-        _resultField.text = _result.ToString();
+        _operationID = 1;
+        ReadInputField1(_inputField1.text);
+        _result = _result + _number1;
+        _inputField2.text = _inputField2.text + _inputField1.text + "+";
+        _inputField1.text = _result.ToString();
+        _screenInputMode = false;
+
     }
-    public void Divide() 
+    public void Minus()
     {
-        if (_number2 != 0)
+        _operationID = 2;
+
+        ReadInputField1(_inputField1.text);
+        if (_result != 0)
         {
-            _result = _number1 / _number2;
-            _resultField.text = _result.ToString();
+            _result = _result - _number1;
+            _inputField2.text = _inputField2.text + _inputField1.text + "-";
         }
-        else 
+        else if (_result == 0)
         {
-            _resultField.text = "You can't divide by zero";
+            _result = _number1 - _result;
+            _inputField2.text = _inputField2.text + _inputField1.text + "-";
+        }
+        _inputField1.text = _result.ToString();
+        _screenInputMode = false;
+        _preOperationID = _operationID;
+    }
+    public void Divide()
+    {
+        _operationID = 3;
+
+        if (_number1 == 0 && _number2 == 0) 
+        {
+            ReadInputField1(_inputField1.text);
+        }
+        else if (_number1 != 0)
+        {
+            _number2 = float.Parse(_inputField1.text);
+        }
+
+        _inputField2.text = _inputField2.text + _inputField1.text + "/";
+
+        Debug.Log("nimber1 = "+_number1);
+        Debug.Log("nimber2 = " + _number2);
+
+        void NewMethod()
+        {
+            ReadInputField1(_inputField1.text);
+            _screenInputMode = false;
         }
     }
     public void Minimum()
@@ -157,4 +176,39 @@ public class Calculator : MonoBehaviour
         _result = Mathf.Pow(_number1, _number2);
         _resultField.text = _result.ToString();
     }
+    public void Polarity ()
+    {
+
+    }
+    public void EqualButtonOnClick()
+    {
+        switch (_operationID)
+        {
+            case 1:
+                _inputField2.text = "";
+                _result = _result + float.Parse(_inputField1.text);
+                _inputField1.text = _result.ToString();
+                _screenInputMode = false;
+                _number1 = 0;
+                _result = 0;
+                Debug.Log(_result);
+                break;
+            case 2:
+                _inputField2.text = "";
+                _result = _result - float.Parse(_inputField1.text);
+                _inputField1.text = _result.ToString();
+                _screenInputMode = false;
+                _number1 = 0;
+                _result = 0;
+                Debug.Log(_result);
+                break;
+            case 3:
+                _result = _number1 / float.Parse(_inputField1.text);
+                _inputField1.text = _result.ToString();
+                _number1 = 0;
+                _result = 0;
+                break;
+        }
+    }
+
 }
